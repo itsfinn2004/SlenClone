29ff1baf856150238c6efc10b257c343781f503e
Finn <fstreunding@gmail.com>
2024-01-18 13:08:55 +0100

    half werkende enemy effect

--- SlenClone/Assets/scripts/EnemyDetection.cs
+++ SlenClone/Assets/scripts/EnemyDetection.cs
@@ -4,41 +4,83 @@ using UnityEngine;
 
 public class EnemyDetection : MonoBehaviour
 {
-    [SerializeField] private LayerMask layerMask;
+    //public GameObject Lasthit;
+    //public Vector3 collision = Vector3.zero;
+    //public float GroteDrawgizmo = 0.2f;
+    //public int MaxLenght = 100;
 
-    void FixedUpdate()
+
+    //private void Update()
+    //{
+    //    var ray = new Ray(this.transform.position, this.transform.forward);
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(ray, out hit, MaxLenght))
+    //    {
+    //        Lasthit = hit.transform.gameObject;
+    //        collision = hit.point;
+    //        Debug.Log(hit.transform.gameObject);
+    //    }
+
+
+
+    //}
+
+    TestController controller;
+    public LayerMask layer;
+    public float coneAngle = 45f;
+    public float detectionRange = 10f;
+    public GameObject enemy;
+
+
+    private void Awake()
     {
-        // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
-        void FixedUpdate()
-        {
-            // Bit shift the index of the layer (8) to get a bit mask
-            int layerMask = 1 << 8;
-
-            // This would cast rays only against colliders in layer 8.
-            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-            layerMask = ~layerMask;
-
-            RaycastHit hit;
-            // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log("Did Hit");
-            }
-            else
-            {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-                Debug.Log("Did not Hit");
-            }
-            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-            Debug.DrawRay(transform.position, forward, Color.green);
-        }
-
-        
-        
-
-
+     
+        controller = enemy.GetComponent<TestController>();
     }
+    void Update()
+    {
+        DetectObjectsInCone();
+    }
+
+    void DetectObjectsInCone()
+    {
+        // Cast rays in a cone pattern
+        for (float angle = -coneAngle / 1; angle <= coneAngle / 1; angle += 5f)
+        {
+            // Calculate direction based on current angle
+            Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+            // Raycast in the calculated direction
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, direction, out hit, detectionRange))
+            {
+                // Check if the hit object is an enemy or something else
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    // Handle enemy detection logic here
+                    
+                    Debug.Log("Enemy detected!");
+                    
+                    controller.CanMove = false;
+                }
+                else if (!hit.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("Enemy NOOOOOT detected");
+                    controller.CanMove = true;
+                }
+                //werkt half omdat de bool van canmove de heletijd switched omdat meerdere rayscasts worden uitgezonden
+            }
+
+            // Visualize the rays (optional)
+            Debug.DrawRay(transform.position, direction * detectionRange, Color.red);
+        }
+    }
+
+
+
+
+
+}
 
   
 
@@ -50,4 +92,4 @@ public class EnemyDetection : MonoBehaviour
 
 
 
-}
+
--- SlenClone/Assets/scripts/TestController.cs
+++ SlenClone/Assets/scripts/TestController.cs
@@ -7,25 +7,39 @@ public class TestController : MonoBehaviour
 
     public Camera cam;
     public NavMeshAgent agent;
-    
+    public Transform player;
+    public float moveSpeed = 5f;
+    public bool CanMove = true;
+
 
     // Update is called once per frame
     void Update()
     {
-      if(Input.GetMouseButtonDown(0))
-      {
-            Debug.Log("mousebuttondown");
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //      Debug.Log("mousebuttondown");
+        //      Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //      RaycastHit hit;
 
-           if (Physics.Raycast(ray, out hit))
-            {
-                
-                Debug.Log("hit");
-                agent.SetDestination(hit.point);
-            }
+        //     if (Physics.Raycast(ray, out hit))
+        //      {
 
-            
-      }
+        //          Debug.Log("hit");
+        //          agent.SetDestination(hit.point);
+        //      }
+
+
+        //}
+        if (CanMove == true)
+        {
+            // Calculate the direction from the enemy to the player
+            Vector3 direction = player.position - transform.position;
+
+            // Normalize the direction to get a unit vector
+            direction.Normalize();
+
+            // Move towards the player
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
     }
 }
